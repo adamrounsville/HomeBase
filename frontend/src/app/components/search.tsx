75ae471e6ee7660dd76a67db7ef6c4ee52110c5d
John Tappen <jtappen@gmail.com>
2024-10-17 21:20:17 -0600

    feat(UI): Incorporated Google Maps Autocomplete into the Search bar. - Created a custom hook that allows the script to be loaded in only once

--- frontend/src/app/components/search.tsx
+++ frontend/src/app/components/search.tsx
@@ -1,13 +1,46 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
- 
+import { Autocomplete, useJsApiLoader } from "@react-google-maps/api";
+import { useState } from "react";
+import { useGoogleMapsLoader } from "./useGoogleMapsLoader";
+
 function SearchBar() {
+  const [searchResult, setSearchResult] = useState<any>()
+
+  const { googleMaps, isLoaded, loadError } = useGoogleMapsLoader();
+
+  function onLoad(autocomplete:any ) {
+    setSearchResult(autocomplete);
+  }
+
+  function onPlaceChanged() {
+    if (searchResult != null) {
+      const place = searchResult.getPlace();
+      const name = place.name;
+      const status = place.business_status;
+      const formattedAddress = place.formatted_address;
+      console.log(`Name: ${name}`);
+      console.log(`Business Status: ${status}`);
+      console.log(`Formatted Address: ${formattedAddress}`);
+    } else {
+      alert("Please enter text");
+    }
+  }
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input type="search" placeholder="Enter a Location" className="homebase-input"/>
-      <Button type="submit">Search</Button>
+        <Autocomplete onPlaceChanged={onPlaceChanged} onLoad={onLoad}>
+          <Input type="text" placeholder="Enter a Location" className="homebase-input"/>
+        </Autocomplete>
+        <Button type="submit">Search</Button>
     </div>
-  )
+  );
 }
 
 export default SearchBar;
