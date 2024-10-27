2ec4556cd299024c7c53889df1776d6b0cb08842
Freddy-Rios <freddyrios2000@gmail.com>
2024-10-27 13:20:28 -0600

    style: updated tailwind css for ellipsis on activity

--- frontend/src/app/components/activitySelector.tsx
+++ frontend/src/app/components/activitySelector.tsx
@@ -39,6 +39,7 @@ const ActivitySelector = ({ activityGroups, setActivityGroups }: props) => {
     setOpenGroup((prevOpenGroup) => (prevOpenGroup === id ? null : id));
   };
 
+  // Old functionality unsure if we will need to refactor to this old version later on so keep for now ??
   // // Function to handle adding new activity group
   // const handleAddActivity = () => {
   //   if (newActivityTitle.trim()) {
@@ -149,7 +150,7 @@ const ActivitySelector = ({ activityGroups, setActivityGroups }: props) => {
                       </span>
                       <DropdownMenu>
                         <DropdownMenuTrigger>
-                          <i className="fa-solid fa-ellipsis cursor-pointer text-gray-500"></i>
+                          <i className="fa-solid fa-ellipsis cursor-pointer text-gray-500 absolute top-2 right-3"></i>
                         </DropdownMenuTrigger>
                         <DropdownMenuContent>
                           <DropdownMenuItem
@@ -176,88 +177,5 @@ const ActivitySelector = ({ activityGroups, setActivityGroups }: props) => {
       </div>
     </>
   );
-
-  // return (
-  //   <>
-  //     <div className="activity-selector-header">
-  //       <h2 className="activity-title">Activities</h2>
-  //       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-  //         <DialogTrigger>
-  //           <i
-  //             className="fas fa-plus"
-  //             onClick={() => setIsDialogOpen(true)}
-  //           ></i>
-  //         </DialogTrigger>
-  //         <DialogContent>
-  //           <DialogHeader>
-  //             <DialogTitle>Add a New Activity Group</DialogTitle>
-  //             <DialogDescription>
-  //               Please enter the title of your new Activity Group.
-  //             </DialogDescription>
-  //             <Input
-  //               value={newActivityTitle}
-  //               onChange={(e) => setNewActivityTitle(e.target.value)}
-  //               placeholder="Enter activity title"
-  //             />
-  //             <Button onClick={handleAddActivity}>Create</Button>
-  //           </DialogHeader>
-  //         </DialogContent>
-  //       </Dialog>
-  //     </div>
-
-  //     <div className="activity-list">
-  //       {activityGroups.map((group) => (
-  //         <div key={group.id} className="activity-group">
-  //           <div className="group-header" onClick={() => toggleGroup(group.id)}>
-  //             <span className="group-title">{group.title}</span>
-  //             <DropdownMenu>
-  //               <DropdownMenuTrigger>
-  //                 <i className="fa-solid fa-ellipsis group-options"></i>
-  //               </DropdownMenuTrigger>
-  //               <DropdownMenuContent>
-  //                 <DropdownMenuItem>Add to Map</DropdownMenuItem>
-  //                 <DropdownMenuSeparator />
-  //                 <DropdownMenuItem>Delete Group</DropdownMenuItem>
-  //               </DropdownMenuContent>
-  //             </DropdownMenu>
-  //           </div>
-
-  //           {openGroup === group.id && (
-  //             <div className="activity-items-container space-y-4 pt-2">
-  //               {group.activities!.map((activity, index) => (
-  //                 <div
-  //                   key={index}
-  //                   className="activity-item relative bg-gray-100 border border-gray-300 p-4 rounded-lg shadow-sm"
-  //                 >
-  //                   <div
-  //                     className="flex justify-between items-center"
-  //                     onClick={() => toggleGroup(group.id)}
-  //                   >
-  //                     <span className="text-xl font-semibold text-gray-800">
-  //                       {activity.Name}
-  //                     </span>
-  //                     <DropdownMenu>
-  //                       <DropdownMenuTrigger>
-  //                         <i className="fa-solid fa-ellipsis cursor-pointer text-gray-500 absolute top-2 right-3"></i>
-  //                       </DropdownMenuTrigger>
-  //                       <DropdownMenuContent className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-  //                         <DropdownMenuItem>Remove</DropdownMenuItem>
-  //                         <DropdownMenuSeparator />
-  //                         <DropdownMenuItem>Add To Daily Plan</DropdownMenuItem>
-  //                       </DropdownMenuContent>
-  //                     </DropdownMenu>
-  //                   </div>
-  //                   <h1 className="text-sm text-gray-600 mb-4">
-  //                     {activity.Address}
-  //                   </h1>
-  //                 </div>
-  //               ))}
-  //             </div>
-  //           )}
-  //         </div>
-  //       ))}
-  //     </div>
-  //   </>
-  // );
 };
 export default ActivitySelector;
